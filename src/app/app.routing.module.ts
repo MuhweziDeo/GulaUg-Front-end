a6da2dac7cb3c4e65d58b10f4c607ede54290d5f
muhwezi deo <aggrey256@gmail.com>
2019-05-08 19:29:56 +0300

    feat(signup): implement signup feature

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,9 +3,11 @@ import {RouterModule, Routes} from '@angular/router';
 
 import { NotFoundComponent } from './not-found/not-found.component';
 import {LandingPageComponent} from './landing-page/landing-page.component';
+import {SignupComponent} from './auth/signup/signup.component';
 
 const routes: Routes = [
   {path: '' , component: LandingPageComponent},
+  {path: 'signup', component: SignupComponent},
   {path: '404', component: NotFoundComponent},
   {path: '**', redirectTo: '/404'}
 ];
