8ae8635ac66a71684f240ce9f851e89fde3daf61
muhwezi deo <aggrey256@gmail.com>
2019-05-25 15:52:10 +0300

    feat(profile): implement profile management for authenticated user

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -5,11 +5,16 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { EmailConfirmationComponent } from './auth/email-confirmation/email-confirmation.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { AuthGuardService } from './auth/auth-guard.service';
 
 const routes: Routes = [
   {path: '' , component: LandingPageComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'auth/verify/:token', component: EmailConfirmationComponent},
+  {path: 'profile', component: ProfileComponent, 
+  canActivate:[AuthGuardService]
+},
   {path: '404', component: NotFoundComponent},
   {path: '**', redirectTo: '/404'}
 ];
---
+++ src/app/user/profile/profile.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.prod';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+  profileUrl: string;
+  constructor(
+    private http: HttpClient
+  ) { 
+    const {apiURl} = environment;
+    this.profileUrl = `${apiURl}auth/`
+  }
+
+  getUserProfile(): Observable<any> {
+    return this.http.get(`${this.profileUrl}user`);
+  }
+  updateProfile(username: string, data:object): Observable<any> {
+    return this.http.put(`${this.profileUrl}profile/${username}`, data)
+  }
+}
