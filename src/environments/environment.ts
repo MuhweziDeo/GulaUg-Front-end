b635e4614ffa6238e88656f9a9286a50fdb9c95f
muhwezi deo <aggrey256@gmail.com>
2019-06-06 19:50:05 +0300

    fix data structuring

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   apiURl: 'https://gula-ug-api.herokuapp.com/',
-  'FacebookId':'378283402776893',
-  'GoogleId':'1049988843166-qkkngs0bhe1uji704dtu62acn4pem6p8.apps.googleusercontent.com'
+  FacebookId: '378283402776893',
+  GoogleId: '1049988843166-qkkngs0bhe1uji704dtu62acn4pem6p8.apps.googleusercontent.com'
 };
 
 /*
